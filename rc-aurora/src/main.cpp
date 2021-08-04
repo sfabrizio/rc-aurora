f2013dc170ed8f3274c988da839289e66f0dd323
Samuel Fabrizio <samuelfabrizio@gmail.com>
2021-08-04 17:27:46 +0200

    [ADD] init project, basic skeleton
         - It's already working, simple version, just printing out on monitor port

---
+++ rc-aurora/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(D0, INPUT); // btn A
+  pinMode(D3, INPUT); // btn B
+  pinMode(D1, INPUT); // btn C
+  pinMode(D2, INPUT); // btn D
+}
+
+void loop() {
+  
+  delay(200);
+
+  if (digitalRead(D0) == HIGH) {
+     Serial.println("d0 wachin A");
+   }
+   if (digitalRead(D3) == HIGH) {
+     Serial.println("d3 wachin B");
+   }
+   if (digitalRead(D1) == HIGH) {
+     Serial.println("d1 wachin C");
+   }
+   if (digitalRead(D2) == HIGH) {
+     Serial.println("d2 wachin D");
+   }
+   
+}
